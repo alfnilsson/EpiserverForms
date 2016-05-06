cd6aebc8cb98e3f170e61f4f02c57a0e223a988c
netr\alf.nilsson <alf.nilsson@netrelations.com>
2016-05-06 22:46:32 +0200

    Added example of an actor

---
+++ Toders.Forms.Web/Business/Forms/PostSubmissionActor.cs
@@ -0,0 +1,13 @@
+﻿using EPiServer.Forms.Core.PostSubmissionActor;
+
+namespace Toders.Forms.Business.Forms
+{
+    public class PostSubmissionActor : PostSubmissionActorBase
+    {
+        public override object Run(object input)
+        {
+            var submissionData = this.SubmissionData;
+            return null;
+        }
+    }
+}
