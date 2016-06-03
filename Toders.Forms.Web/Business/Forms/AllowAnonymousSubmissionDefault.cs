e84cd31dc55817b8f7195a30ccac064c71d3287e
mail@alfnilsson.se <mail@alfnilsson.se>
2016-06-03 17:44:40 +0200

    Allow anonymous submission

---
+++ Toders.Forms.Web/Business/Forms/AllowAnonymousSubmissionDefault.cs
@@ -0,0 +1,34 @@
+﻿using EPiServer;
+using EPiServer.Core;
+using EPiServer.Forms.Implementation.Elements;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+
+namespace Toders.Forms.Business.Forms
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class AllowAnonymousSubmissionDefault : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            ServiceLocator.Current.GetInstance<IContentEvents>().CreatingContent += SetAllowAnonymousSubmission;
+        }
+
+        private void SetAllowAnonymousSubmission(object sender, ContentEventArgs e)
+        {
+            var formContainerBlock = e.Content as FormContainerBlock;
+            if (formContainerBlock == null)
+            {
+                return;
+            }
+
+            formContainerBlock.AllowAnonymousSubmission = true;
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+    }
+}
