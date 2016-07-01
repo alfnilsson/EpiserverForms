60f06c820d99e44657a9a715925f3ab163826dc8
Alf Nilsson <alf.nilsson@netrelations.com>
2016-07-01 14:27:31 +0200

    Added how to find the friendly names

--- Toders.Forms.Web/Business/Forms/FormReader.cs
+++ Toders.Forms.Web/Business/Forms/FormReader.cs
@@ -1,31 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using EPiServer;
 using EPiServer.Core;
 using EPiServer.Forms.Core;
 using EPiServer.Forms.Core.Data;
 using EPiServer.Forms.Core.Models;
-using EPiServer.Forms.Helpers;
+using EPiServer.Forms.Core.Models.Internal;
 using EPiServer.ServiceLocation;
 
-namespace Toders.Forms.Business.Forms
+namespace Toders.Web.Business.Forms
 {
     public class FormReader
     {
+        private Injected<IFormRepository> _formRepository;
         private Injected<IFormDataRepository> formDataRepository;
+        private Injected<IContentLoader> contentLoader;
 
-        public IEnumerable<Submission> ReadForm(ContentReference contentLink)
+        public FormInformation ReadForm(ContentReference contentLink)
         {
-            IContent content = contentLink.GetContent();
+            IContent content = contentLoader.Service.Get<IContent>(contentLink);
             ILocalizable localizable = content as ILocalizable;
             if (content == null || localizable == null)
             {
-                return Enumerable.Empty<Submission>();
+                return FormInformation.Empty;
             }
 
             FormIdentity formIdentity = new FormIdentity(content.ContentGuid, localizable.Language.Name);
             IEnumerable<Submission> submissions = formDataRepository.Service.GetSubmissionData(formIdentity, DateTime.MinValue, DateTime.MaxValue);
-            return submissions;
+            IEnumerable<FriendlyNameInfo> friendlyNameInfos = this._formRepository.Service.GetDataFriendlyNameInfos(formIdentity);
+
+            var info = new FormInformation
+            {
+                Submissions = submissions,
+                FriendlyNameInfos = friendlyNameInfos
+            };
+
+            return info;
         }
+    }
+
+    public class FormInformation
+    {
+        public static FormInformation Empty
+        {
+            get
+            {
+                return new FormInformation
+                {
+                    Submissions = Enumerable.Empty<Submission>()
+                };
+            }
+        }
+
+        public IEnumerable<Submission> Submissions { get; set; }
+
+        // formInformation.FriendlyNameInfos.First(x => x.ElementId == currentElementId)
+        // Where currentElementId could be __field__123
+        public IEnumerable<FriendlyNameInfo> FriendlyNameInfos { get; set; }
     }
 }
